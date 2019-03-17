c983f0a2df800a5ceb20d494be6b1170d33ba228
Tim Zöller <tim.zoeller@ilume.de>
2019-03-17 12:23:17 +0100

    Initial commit

---
+++ customers/src/main/java/de/javahippie/backinthering/mp/dg/MpdgRestApplication.java
@@ -0,0 +1,19 @@
+package de.javahippie.backinthering.mp.dg;
+
+import org.eclipse.microprofile.auth.LoginConfig;
+
+import javax.annotation.security.DeclareRoles;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+
+@LoginConfig(authMethod = "MP-JWT", realmName = "jwt-jaspi")
+@DeclareRoles({"protected"})
+
+public class MpdgRestApplication extends Application {
+}
---
+++ service/src/main/java/de/javahippie/backinthering/service/MpwarehouseRestApplication.java
@@ -0,0 +1,19 @@
+package de.javahippie.backinthering.service;
+
+import org.eclipse.microprofile.auth.LoginConfig;
+
+import javax.annotation.security.DeclareRoles;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+
+@LoginConfig(authMethod = "MP-JWT", realmName = "jwt-jaspi")
+@DeclareRoles({"protected"})
+
+public class MpwarehouseRestApplication extends Application {
+}
